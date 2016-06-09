1dd1a4fafff87e3f678adc90d84aad7ebb3e9d96
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-06-09 02:15:11 +0200

    Com prototype refactored to discover common code.

--- C/wspolbie/zad-project/com_client.c
+++ C/wspolbie/zad-project/com_client.c
@@ -8,32 +8,32 @@
 void main(){
 	
 	printf("Witaj\n");
-	
+
+	int ip4addrSize;
+	ip4addrSize = sizeof(struct sockaddr_in);
+
+	int s;
+	s = socket(PF_INET, SOCK_DGRAM, 0x0);
+
 	struct sockaddr_in ip4addr;
 	bzero((char *) &ip4addr, sizeof(ip4addr));
 	ip4addr.sin_family = AF_INET;
 	ip4addr.sin_port = htons((ushort) 5000);
 	inet_pton(AF_INET, "192.168.1.143", &ip4addr.sin_addr);
 
-	int fromlen;
-	fromlen = sizeof(struct sockaddr_in);
+	bind(s, (struct sockaddr*) &ip4addr, ip4addrSize);
 
 	int msg;
 	printf("Podaj liczbe: ");
 	scanf("%d", &msg);
 
-	int s;
-	s = socket(PF_INET, SOCK_DGRAM, 0);
-
-	bind(s, (struct sockaddr*) &ip4addr, fromlen);
-
 	int buff = htonl(msg);
-	sendto(s, (char *)&buff, sizeof(buff), 0, (struct sockaddr*) &ip4addr, fromlen);
+	sendto(s, (char *)&buff, sizeof(buff), 0x0, (struct sockaddr*) &ip4addr, ip4addrSize);
 
 	printf("Wiadomosc wyslana: [%d]\n", msg);
 
 	int byte_count;
-	byte_count = recvfrom(s, (char *)&buff, sizeof(buff), 0, (struct sockaddr*) &ip4addr, &fromlen);
+	byte_count = recvfrom(s, (char *)&buff, sizeof(buff), 0x0, (struct sockaddr*) &ip4addr, &ip4addrSize);
 	buff = ntohl(buff);
 	
 	printf("Liczba: %d\n", buff);
--- C/wspolbie/zad-project/com_server.c
+++ C/wspolbie/zad-project/com_server.c
@@ -8,26 +8,26 @@
 void main(){
 	
 	printf("Witaj\n");
-	
-	struct sockaddr_in ip4addr;
-	bzero((char *) &ip4addr, sizeof(ip4addr));
-	ip4addr.sin_family = AF_INET;
-	ip4addr.sin_port = htons((ushort) 5000);
-	ip4addr.sin_addr.s_addr=htonl(INADDR_ANY);
 
-	int fromlen;
-	fromlen = sizeof(struct sockaddr_in);
+	int ip4addrSize;
+	ip4addrSize = sizeof(struct sockaddr_in);
 
 	int s;
 	s = socket(PF_INET, SOCK_DGRAM, 0x0);
 
-	bind(s, (struct sockaddr*) &ip4addr, fromlen);
+	struct sockaddr_in ip4addr;
+	bzero((char *) &ip4addr, sizeof(ip4addr));
+	ip4addr.sin_family = AF_INET;
+	ip4addr.sin_port = htons((ushort) 5000);
+	ip4addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	bind(s, (struct sockaddr*) &ip4addr, ip4addrSize);
 
 	while(1){
 		int buff;
 		int byte_count;
 		
-		byte_count = recvfrom(s, (char *)&buff, sizeof(buff), 0x0, (struct sockaddr*) &ip4addr, &fromlen);
+		byte_count = recvfrom(s, (char *)&buff, sizeof(buff), 0x0, (struct sockaddr*) &ip4addr, &ip4addrSize);
 		buff = ntohl(buff);
 		
 		printf("Liczba: %d\n", buff);
@@ -40,7 +40,7 @@ void main(){
 		printf("Wiadomosc wyslana: [%d]\n", buff);
 		
 		buff = htonl(buff);
-		sendto(s, (char *)&buff, sizeof(buff), 0x0, (struct sockaddr*) &ip4addr, fromlen);
+		sendto(s, (char *)&buff, sizeof(buff), 0x0, (struct sockaddr*) &ip4addr, ip4addrSize);
 	}
 }           
        
